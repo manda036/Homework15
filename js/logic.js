06b4b9c98139286d6a15f9654f5b9084151ca63f
Mamta <mamta.mandan@gmail.com>
2019-01-14 21:59:57 -0600

    updated logic.js with function inside d3.json

--- js/logic.js
+++ js/logic.js
@@ -10,36 +10,45 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   accessToken: API_KEY
 }).addTo(myMap);
   
-function createMarkers(response) {
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(response) {
   var magnitudes = response.features[0].properties.mag;
+  console.log(magnitudes);
   var locations = [[response.features[0].geometry.coordinates[1], response.features[0].geometry.coordinates[0]]];
+  console.log(locations);
   var places = response.features[0].properties.place;
+  console.log(places);
   var earthquakeMarkers = [];
   for (var index = 0; index < magnitudes.length; index++) {
     var color = "";
     if (magnitudes < 2.5) {
-      color = "yellow";
-    }
+        color = "yellow";
+      }
     else if (magnitudes = 2.6 < 4.5) {
       color = "orange";
-    }
+      }
     else {
       color = "red";
+      }
+      
+      var location = locations[index];
+      var magnitude = magnitudes[index];
+      var place = places[index];
+      function earthquakeMarker() {L.circle(location, {
+        fillOpacity: 0.75,
+        color: "white",
+        fillColor: color,
+        radius: magnitude
+      });
     }
-    
-    var location = locations[index];
-    var magnitude = magnitudes[index];
-    var place = places[index];
-    var earthquakeMarker = L.circle(location, {
-      fillOpacity: 0.75,
-      color: "white",
-      fillColor: color,
-      radius: magnitude
-    }).bindPopup("<h3>" + place + "<h3><h3>" + magnitude + "<h3>");
-    earthquakeMarkers.push(earthquakeMarker);
+      
+      L.geoJson(data, {
+        pointToLayer: function(feature, location) {
+          return L.circleMarker(location)
+        },
+        style: earthquakeMarker,
+        onEachFeature: function (feature, layer) {
+      layer.bindPopup("<h3>" + place + "<h3><h3>" + magnitude + "<h3>");
+        }
+      }).addTo(map);
     }
-    
-    L.layerGroup(earthquakeMarkers).addTo(myMap);
-  }
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(data=>createMarkers(data));
-  
+  });
