3bbec28417cf0add5d7f87260e72170957111caf
Mamta <mamta.mandan@gmail.com>
2019-01-31 10:11:55 -0600

    fixed most bugs in code to render circles

--- js/logic.js
+++ js/logic.js
@@ -11,28 +11,34 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 }).addTo(myMap);
   
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(response) {
-  var magnitudes = response.features[0].properties.mag;
+  // console.log(response.features);
+  var magnitudes = [];
+  var locations = [];
+  var places = [];
+  for (var index = 0; index < response.features.length; index++) {
+  magnitudes.push(response.features[index].properties.mag);
+  locations.push([[response.features[index].geometry.coordinates[1], response.features[index].geometry.coordinates[0]]]);
+  places.push(response.features[index].properties.place);
+  }
   console.log(magnitudes);
-  var locations = [[response.features[0].geometry.coordinates[1], response.features[0].geometry.coordinates[0]]];
   console.log(locations);
-  var places = response.features[0].properties.place;
   console.log(places);
-  var earthquakeMarkers = [];
+  // var earthquakeMarkers = [];
   for (var index = 0; index < magnitudes.length; index++) {
     var color = "";
-    if (magnitudes < 2.5) {
+    if (magnitude < 2.5) {
         color = "yellow";
       }
-    else if (magnitudes = 2.6 < 4.5) {
+    else if (magnitude = 2.6 < 4.5) {
       color = "orange";
       }
     else {
       color = "red";
       }
       
-      var location = locations[index];
-      var magnitude = magnitudes[index];
-      var place = places[index];
+      var location = locations[1];
+      var magnitude = magnitudes[1];
+      var place = places[1];
       function earthquakeMarker() {L.circle(location, {
         fillOpacity: 0.75,
         color: "white",
@@ -41,7 +47,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
       });
     }
       
-      L.geoJson(data, {
+      L.geoJson(response, {
         pointToLayer: function(feature, location) {
           return L.circleMarker(location)
         },
@@ -49,6 +55,6 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         onEachFeature: function (feature, layer) {
       layer.bindPopup("<h3>" + place + "<h3><h3>" + magnitude + "<h3>");
         }
-      }).addTo(map);
+      }).addTo(myMap);
     }
   });
