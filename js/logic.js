48a5de7fa040983c02bd62779fa345aa3f1dd563
Mamta <mamta.mandan@gmail.com>
2019-02-02 13:29:54 -0600

    completed!

--- js/logic.js
+++ js/logic.js
@@ -11,50 +11,74 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 }).addTo(myMap);
   
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(response) {
-  // console.log(response.features);
-  var magnitudes = [];
-  var locations = [];
-  var places = [];
-  for (var index = 0; index < response.features.length; index++) {
-  magnitudes.push(response.features[index].properties.mag);
-  locations.push([[response.features[index].geometry.coordinates[1], response.features[index].geometry.coordinates[0]]]);
-  places.push(response.features[index].properties.place);
-  }
-  console.log(magnitudes);
-  console.log(locations);
-  console.log(places);
-  // var earthquakeMarkers = [];
-  for (var index = 0; index < magnitudes.length; index++) {
-    var color = "";
+  console.log(response.features);
+  // var magnitudes = [];
+  // var locations = [];
+  // var places = [];
+  // for (var index = 0; index < response.features.length; index++) {
+  // magnitudes.push(response.features[index].properties.mag);
+  // locations.push([response.features[index].geometry.coordinates[1], response.features[index].geometry.coordinates[0]]);
+  // places.push(response.features[index].properties.place);
+  // }
+  // console.log(magnitudes);
+  // console.log(locations);
+  // console.log(places);
+  function getColor (magnitude) {
+    // var color = "";
     if (magnitude < 2.5) {
-        color = "yellow";
+        return "#FFFF00";
       }
-    else if (magnitude = 2.6 < 4.5) {
-      color = "orange";
+    else if (magnitude < 4.5) {
+      return "#FFA500";
       }
     else {
-      color = "red";
+      return "#FF0000";
       }
-      
-      var location = locations[1];
-      var magnitude = magnitudes[1];
-      var place = places[1];
-      function earthquakeMarker() {L.circle(location, {
-        fillOpacity: 0.75,
-        color: "white",
-        fillColor: color,
-        radius: magnitude
-      });
+    };
+    
+      // var location = locations[0];
+      // var magnitude = magnitudes[0];
+      // var place = places[0];
+      function earthquakeMarker(feature) {
+        return {
+        fillOpacity: 1,
+        color: "#000000",
+        fillColor: getColor(feature.properties.mag),
+        stroke: true,
+        weight: 0.5,
+        opacity: 1,
+        radius: feature.properties.mag*1.5
+      };
     }
-      
+    var legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function () {
+    
+      var div = L.DomUtil.create('div', 'info legend')
+      grades = [0, 2.5, 4.5];
+      var colorss = ["0", "1", "#FF0000"];
+        // labels = [];
+    
+        // loop through our density intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                "<i style='background: "+ colorss[0] + "'></i> "+
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+    
+    return div;
+    };
+    
+    legend.addTo(myMap);
+
       L.geoJson(response, {
         pointToLayer: function(feature, location) {
           return L.circleMarker(location)
         },
         style: earthquakeMarker,
         onEachFeature: function (feature, layer) {
-      layer.bindPopup("<h3>" + place + "<h3><h3>" + magnitude + "<h3>");
+      layer.bindPopup("<h3>" + feature.properties.place + "<h3><h3>" + feature.properties.mag + "<h3>");
         }
       }).addTo(myMap);
-    }
-  });
+    });
+  // });
